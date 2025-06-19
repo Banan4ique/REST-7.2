13887f173d650107a2e287da67c8667bdbd4536f
Denis Lipatov <nutela113@yandex.ru>
2025-06-20 00:23:51 +0300

    Added validation

--- src/main/java/ru/netology/rest/controller/AuthorizationController.java
+++ src/main/java/ru/netology/rest/controller/AuthorizationController.java
@@ -1,6 +1,6 @@
 package ru.netology.rest.controller;
 
-import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.rest.DTO.User;
@@ -19,7 +19,7 @@ public class AuthorizationController {
     }
 
     @GetMapping("/authorize")
-    public List<Authorities> getAuthorities(@Valid @SplitIntoUserAndPassword User user) {
+    public List<Authorities> getAuthorities(@SplitIntoUserAndPassword @Validated User user) {
         return service.getAuthorities(user);
     }
 }
